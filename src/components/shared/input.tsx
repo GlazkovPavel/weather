cd389d490efb4e6424de351a6e0cc07d72fd8e6f
Pavel Glazkov <glazprom@mail.ru>
2023-01-16 18:44:07 +0300

    Revert "first commit"
    This reverts commit da3ff05de94fd013793c30c0ba8845fa527ddba3.

--- src/components/shared/input.tsx
+++
@@ -1,9 +0,0 @@
-import { ChangeEventHandler } from "react";
-
-function Input(props: { onChange: ChangeEventHandler<HTMLInputElement> | undefined; }) {
-    return(
-        <input onChange={props.onChange} className='input__search'/>
-    )
-}
-
-export default Input;
